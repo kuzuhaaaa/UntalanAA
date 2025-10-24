c968987fc294463ba82fe3b208f0f86b098efa3f
kuzuhaaaa <135844188+kuzuhaaaa@users.noreply.github.com>
2025-10-24 17:23:26 +0800

    Update delete_driver.php

--- delete_driver.php
+++ delete_driver.php
@@ -1,6 +1,12 @@
 <?php
 include 'db.php';
+
 $id = $_GET['id'];
+
+$conn->query("DELETE FROM results WHERE driver_id = $id");
+
 $conn->query("DELETE FROM drivers WHERE id = $id");
+
 header("Location: index.php");
+exit;
 ?>
